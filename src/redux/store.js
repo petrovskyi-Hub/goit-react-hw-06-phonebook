d416e36774c415eb545b730d0a2c7c0aecf76dc2
Yaroslav Petrovskyi <yarikpetrovsk@gmail.com>
2021-01-19 23:42:34 +0200

    add combineReducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import shortid from 'shortid';
 import storage from '../services/StorageServices';
 
-const initialState = {
-  contacts: {
-    items: storage.load('Contacts'),
-    filter: '',
-  },
-};
-
-const reducer = (state = initialState, { type, payload }) => {
+const itemsReducer = (state = storage.load('Contacts'), { type, payload }) => {
   switch (type) {
     case 'contacts/addContact':
       const contact = {
@@ -19,37 +12,35 @@ const reducer = (state = initialState, { type, payload }) => {
         number: payload.number,
       };
 
-      return {
-        ...state,
-        contacts: {
-          ...state.contacts,
-          items: [...state.contacts.items, contact],
-        },
-      };
+      return [...state, contact];
 
     case 'contacts/deleteContact':
-      return {
-        ...state,
-        contacts: {
-          ...state.contacts,
-          items: state.contacts.items.filter(contact => contact.id !== payload),
-        },
-      };
-
-    case 'filter/setFilter':
-      return {
-        ...state,
-        contacts: {
-          ...state.contacts,
-          filter: payload,
-        },
-      };
+      return state.filter(contact => contact.id !== payload);
 
     default:
       return state;
   }
 };
 
-const store = createStore(reducer, composeWithDevTools());
+const filterReducer = (state = '', { type, payload }) => {
+  switch (type) {
+    case 'filter/setFilter':
+      return payload;
+
+    default:
+      return state;
+  }
+};
+
+const contactsReduucer = combineReducers({
+  items: itemsReducer,
+  filter: filterReducer,
+});
+
+const rootReduucer = combineReducers({
+  contacts: contactsReduucer,
+});
+
+const store = createStore(rootReduucer, composeWithDevTools());
 
 export default store;
